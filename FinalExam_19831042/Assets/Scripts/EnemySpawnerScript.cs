1adff9fcdc5b811bb67e9ef7d8c61945ecb3355f
Box <tony2yoo@gmail.com>
2022-06-20 02:44:00 +0900

    [update] Background, random spawn, hp fuel guage status

--- FinalExam_19831042/Assets/Scripts/EnemySpawnerScript.cs
+++ FinalExam_19831042/Assets/Scripts/EnemySpawnerScript.cs
@@ -10,6 +10,9 @@ public class EnemySpawnerScript : MonoBehaviour
     public float coolTime = 2f;
     public static float chance = 15;
 
+    int rndX;
+    int[] posX = { -6, -4, -2, 2, 4, 6 };
+    int[] number = new int[6];
     // Update is called once per frame
     void Update()
     {
@@ -25,8 +28,32 @@ public class EnemySpawnerScript : MonoBehaviour
             }
             else
             {
-                Instantiate(item, transform.position,transform.rotation);
+                Instantiate(item, transform.position, transform.rotation);
             }
+
+            // 스폰 포지션에 대한 랜덤 검사 부분
+            int rndZ = Random.Range(50, 90);
+
+            for (int i = 0; i < 6; i++)
+            {
+                number[i] = Random.Range(0, 6);
+            }
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                for(int j = i + 1; j <number.Length; j++)
+                {
+                    if(number[i] == number[j] && i != j)
+                    {
+                        number[i] = Random.Range(0, 6);
+                        rndX = posX[number[i]];
+                        j = -1;
+                    }
+                }
+            }
+            transform.position = new Vector3(rndX, 0.5f, rndZ);
+            
         }
+
     }
 }
--- FinalExam_19831042/Assets/Scripts/PlayerSciript.cs
+++ FinalExam_19831042/Assets/Scripts/PlayerSciript.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PlayerSciript : MonoBehaviour
 {
     public float limitX, speed;
+    public float playerHP = 100;
+    public float fuel = 50;
+    public float fuelDifficulty = 1;
+    float currentTime = 0;
+
+    public GameObject hpGuage;
+    public GameObject fuelGuage;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -26,12 +37,30 @@ public class PlayerSciript : MonoBehaviour
 
         if (transform.position.x < -limitX )
         {
-            transform.position = new Vector3(-limitX, 0.5f, -15);
+            transform.position = new Vector3(-limitX, 0.5f, -20);
         }
         else if (transform.position.x > limitX)
         {
-            transform.position = new Vector3(limitX, 0.5f, -15);
+            transform.position = new Vector3(limitX, 0.5f, -20);
         }
+
+        hpGuage.GetComponent<Slider>().value = playerHP;
+
+        currentTime += Time.deltaTime;
+        if(currentTime >= fuelDifficulty)
+        {
+            fuel -= currentTime;
+            currentTime = 0;
+        }
+        fuelGuage.GetComponent<Slider>().value = fuel;
+        
+        if(fuel <= 0 || playerHP <= 0)
+        {
+            
+            //todo 메인화면으로 돌아가기
+        }
+    
+    
     }
 
     private void OnTriggerEnter(Collider other)
@@ -40,11 +69,27 @@ public class PlayerSciript : MonoBehaviour
         {
             Debug.Log("item");
             Destroy(other.gameObject);
+            if(fuel >= 0)
+            {
+                fuel += 0.5f;
+            }
+        }
+        else if (other.gameObject.CompareTag("")) // 수리 스페어 자리
+        {
+            Destroy(other.gameObject);
+            if(playerHP < 100)
+            {
+                playerHP += 5;
+            }
         }
         else if (other.gameObject.CompareTag("Enemy"))
         {
             Debug.Log("Enemy");
             Destroy(other.gameObject);
+            if (playerHP >= 0)
+            {
+                playerHP -= 10;
+            }
         }
         else
         {
